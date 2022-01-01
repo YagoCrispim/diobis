4e984de924434c17db23d302a74c46b6e55abd57
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-01 19:34:48 -0300

    add: Cred box on PC view

--- components/Creds/Creds.jsx
+++ components/Creds/Creds.jsx
@@ -12,56 +12,58 @@ const Cred = forwardRef(({ showTitle, noAnimation, visible }, ref) => {
 
   return (
     <CSS.Container ref={localRef} noAnimation={noAnimation} visible={visible}>
-      <CSS.Cred>
-        {showTitle && <CSS.Title>Desenvolvido por</CSS.Title>}
-        <CSS.Infos>
-          <CSS.Name>
-            <span>Yago Crispim</span>
-          </CSS.Name>
-          <div>
-            <a
-              href="https://www.linkedin.com/in/yago-crispim-66b01619b/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <LinkedIn />
-            </a>
-            <a
-              href="https://github.com/YagoCrispim"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <GitHub />
-            </a>
-          </div>
-        </CSS.Infos>
+      <CSS.Content>
+        <CSS.Cred>
+          {showTitle && <CSS.Title>Desenvolvido por</CSS.Title>}
+          <CSS.Infos>
+            <CSS.Name>
+              <span>Yago Crispim</span>
+            </CSS.Name>
+            <div>
+              <a
+                href="https://www.linkedin.com/in/yago-crispim-66b01619b/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <LinkedIn />
+              </a>
+              <a
+                href="https://github.com/YagoCrispim"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <GitHub />
+              </a>
+            </div>
+          </CSS.Infos>
 
-        <CSS.Separator>
-          <div></div>
-        </CSS.Separator>
+          <CSS.Separator>
+            <div></div>
+          </CSS.Separator>
 
-        <CSS.Infos>
-          <CSS.Name>
-            <span>Washington Jr.</span>
-          </CSS.Name>
-          <div>
-            <a
-              href="https://www.linkedin.com/in/washington-junior-7136a8101/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <LinkedIn />
-            </a>
-            <a
-              href="https://github.com/washingtonj"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <GitHub />
-            </a>
-          </div>
-        </CSS.Infos>
-      </CSS.Cred>
+          <CSS.Infos>
+            <CSS.Name>
+              <span>Washington Jr.</span>
+            </CSS.Name>
+            <div>
+              <a
+                href="https://www.linkedin.com/in/washington-junior-7136a8101/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <LinkedIn />
+              </a>
+              <a
+                href="https://github.com/washingtonj"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <GitHub />
+              </a>
+            </div>
+          </CSS.Infos>
+        </CSS.Cred>
+      </CSS.Content>
     </CSS.Container>
   )
 })
--- components/Creds/Creds.styled.js
+++ components/Creds/Creds.styled.js
@@ -2,104 +2,71 @@ import styled, { keyframes, css } from 'styled-components'
 import tw from 'twin.macro'
 
 export const Container = styled.div`
-  ${tw`flex fixed left-0 bottom-0 bg-white shadow-2xl p-2 justify-center items-center w-full`}
-  height: 2.5rem;
-  background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(15px);
-  -webkit-backdrop-filter: blur(15px);
+  ${tw`fixed bg-white flex justify-center items-center mt-6 top-12 z-50`}
+  -webkit-box-shadow: 0px 5px 10px 0px rgba(50, 50, 50, 0.56);
+  -moz-box-shadow: 0px 5px 10px 0px rgba(50, 50, 50, 0.56);
+  box-shadow: 0px 5px 10px 0px rgba(50, 50, 50, 0.56);
+  height: 15%;
+  width: 80%;
+  right: 5rem;
+  max-width: 250px;
+  min-height: 200px;
   border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.18);
-  z-index: 2;
 
-  @media (max-width: 1001px) {
-    ${tw`fixed left-auto bg-white mt-6 top-12 z-50`}
-    -webkit-box-shadow: 0px 5px 10px 0px rgba(50, 50, 50, 0.56);
-    -moz-box-shadow: 0px 5px 10px 0px rgba(50, 50, 50, 0.56);
-    box-shadow: 0px 5px 10px 0px rgba(50, 50, 50, 0.56);
-    height: 15%;
-    width: 80%;
-    right: 5rem;
-    max-width: 250px;
-    min-height: 200px;
+  animation: ${({ visible, noAnimation }) =>
+    noAnimation
+      ? css`
+          ${hide} 0s forwards;
+        `
+      : visible && !noAnimation
+      ? css`
+          ${show} 0.2s forwards;
+        `
+      : css`
+          ${hide} 0.2s forwards;
+        `};
+`
 
-    animation: ${({ visible, noAnimation }) =>
-      noAnimation
-        ? css`
-            ${hide} 0s forwards;
-          `
-        : visible && !noAnimation
-        ? css`
-            ${show} 0.2s forwards;
-          `
-        : css`
-            ${hide} 0.2s forwards;
-          `};
-  }
+
+export const Content = styled.div`
+  flex: 1;
+  margin: 10px;
 `
 
 export const Cred = styled.div`
-  ${tw`flex flex-row justify-center items-center`}
-  width: 670px;
-
-  // Cred mobile
-  @media (max-width: 1001px) {
-    ${tw`flex flex-col w-full items-start`}
-  }
+  ${tw`flex flex-col w-full items-start`}
 `
 
 export const Separator = styled.div`
-  ${tw`hidden`}
+  ${tw`flex w-full justify-center`}
 
-  @media (max-width: 1001px) {
-    ${tw`flex w-full justify-center`}
-
-    & div {
-      ${tw`flex justify-center w-3/4 bg-gray-300`}
-      height: 1px;
-      margin-top: 10px;
-      margin-bottom: 10px;
-    }
+  & div {
+    ${tw`flex justify-center w-3/4 bg-gray-300`}
+    height: 1px;
+    margin-top: 10px;
+    margin-bottom: 10px;
   }
 `
 
 export const Title = styled.div`
-  ${tw`flex flex-row justify-start text-gray-600 text-lg font-semibold pl-4 items-center w-full`}
-
-  @media (max-width: 1001px) {
-    ${tw`mb-3`}
-  }
+  ${tw`w-full mb-3`};
+  font-size: 1.2rem;
 `
 
 export const Infos = styled.div`
-  ${tw`flex flex-row justify-around px-14 items-center`}
-  width: 100%;
-
-  & :last-child {
-    content: '|';
-  }
-
-  /* Container com infos */
-  @media (max-width: 1001px) {
-    ${tw`flex w-full p-0 justify-start`}
-  }
+  ${tw`flex w-full items-center`}
+  width: 95%;
+  margin-left: 5px;
 
   // nomes
   & div:first-child {
-    /* background-color: yellow; */
-    @media (max-width: 1001px) {
-      width: 70%;
-
-      & span {
-        display: flex;
-        justify-content: flex-start;
-      }
-    }
+    width: 80%;
   }
 
   // icons
   & div:last-child {
-    ${tw`flex w-1/2 justify-center`}
+    ${tw`flex`}
+    width: 30%;
 
     & svg {
       transition: 0.2s;
@@ -116,34 +83,15 @@ export const Infos = styled.div`
         filter: invert(0.5);
       }
     }
-
-    @media (max-width: 1001px) {
-      ${tw`flex`}
-      width: 30%;
-
-      /* FIX */
-      margin-left: -15px;
-    }
   }
 `
 
 export const Name = styled.div`
-  ${tw`flex justify-center w-full`}
-  padding-left: 10px;
-  padding-right: 10px;
-
-  @media (max-width: 1001px) {
-    /* ${tw`bg-blue-600`} */
-  }
+  height: 100%;
 
   & span {
-    ${tw`flex text-base`}
+    ${tw`flex justify-start items-center text-base text-lg`}
     color: rgb(55, 65, 81);
-    justify-content: center;
-
-    @media (max-width: 1001px) {
-      ${tw`flex flex-row w-full text-lg items-start`}
-    }
   }
 `
 
